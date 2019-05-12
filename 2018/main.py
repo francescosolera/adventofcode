9b4f9cc96ceab42715873dec62c7feece710736f
Francesco Solera <francesco.solera@gmail.com>
2019-05-12 16:34:15 +0200

    day 9

--- 2018/main.py
+++ 2018/main.py
@@ -169,23 +169,20 @@ def day8(input_file_path):
 
 def day9(input_file_path):
 	"""DAY 9 - 10 LINES - https://adventofcode.com/2018/day/9
-
-	If a function terminates without a return value, it returns None, as in def "next".
-	Such function returns the first element in the alphabet which is not done and not ongoing and has no
-	unresolved dependencies linking to it. Ongoing work can be defined as all tasks assigned to elves
-	and not found in o2. The line starting with "if next(" could be greatly simplified with assignment
-	expressions introduced in python 3.8 PEP 572.
+	
+	I first tried a solution with list (insert and del at index) but it was way much slower.
+	Deques with rotate has to be preferred in this case because append and pop are much faster.
 	"""
-	elves, o2, r1, r2, data = [{'t': '.', 'r': -1} for _ in range(5)], '.', '', -1, tuple(map(lambda x: re.search(r'Step (?P<a>\w+) must be finished before step (?P<b>\w+) can begin.', x).groupdict(), open(input_file_path, 'r')))
-	def next(done, ongoing):
-		for a in sorted(set([d for dd in data for d in dd.values()])-set(done)-set(ongoing)):
-			if sum(1 for x in data if x['b'] == a and x['a'] not in done) == 0: return a
-	while next(r1, '') is not None: r1 += next(r1, '')
-	while not all([e['t'] in o2 and e['t'] != '.' for e in elves]):
-		for e in [elf for elf in elves if elf['r'] == 0]: o2 += e['t']
-		for e in [elf for elf in elves if elf['t'] in o2]:
-			if next(o2, ''.join([e['t'] for e in elves if e['t'] not in o2])) is not None: e['t'], e['r'] = next(o2, ''.join([e['t'] for e in elves if e['t'] not in o2])), 60 + ord(next(o2, ''.join([e['t'] for e in elves if e['t'] not in o2]))) - ord('A') + 1
-		for e in elves: e['r'], r2 = e['r']-1, r2+1 if e == elves[-1] else r2
+	data = re.search(r'(?P<n_players>\w+) players; last marble is worth (?P<n_points>\w+) points', open(input_file_path, 'r').read()).groupdict()
+	game, players, r1, r2 = deque([0]), defaultdict(int), 0, 0
+	for marble in range(1, 100 * int(data['n_points']) + 1):
+		game.rotate(-1)
+		if marble % 23 == 0:
+			game.rotate(8)
+			players[marble % int(data['n_players'])] += marble + game.pop()
+			game.rotate(-1)
+		else: game.append(marble)
+		r1, r2 = max(players.values()) if marble == int(data['n_points']) else r1, max(players.values()) if marble == 100 * int(data['n_points']) else r2
 	return r1, r2
 
 
