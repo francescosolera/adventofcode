bbc2d6607bbb561f37400772c5b5c5a8e42510fc
Francesco Solera <francesco.solera@deepvisionconsulting.com>
2021-12-08 01:26:07 +0100

    fixed return lines

--- 2021/main.py
+++ 2021/main.py
@@ -5,7 +5,7 @@ import numpy as np
 import re
 
 '''DESIGN RULE OF THUMBS:
-- keep all functions below 10 lines of code (def line and return line excluded)
+- keep all functions equal or below 10 lines of code (def line and return line excluded)
     - def line is always: def dayX(input_file_path)
     - return line is always: return r1, r2
 - no solutions should be unreasonably slow
@@ -17,29 +17,27 @@ import re
 
 
 def day1(input_file_path):
-    """DAY 1 - 4 LINES - https://adventofcode.com/2021/day/1
+    """DAY 1 - 3 LINES - https://adventofcode.com/2021/day/1
 
     Compunting the difference of a sum/average over a sliding window let us cancel out the terms shared by the two
     windows; in the case of a 3x1 windows two terms out of three are shared.
     """
     data = np.array(tuple(map(int, open(input_file_path, 'r'))))
-    def count_neg_diff_k(raw, k):
-        return sum(raw[k:] - raw[0:-k] > 0)
-    return count_neg_diff_k(data, 1), count_neg_diff_k(data, 3)
+    def count_neg_diff_k(raw, k): return sum(raw[k:] - raw[0:-k] > 0)
+    r1, r2 = count_neg_diff_k(data, 1), count_neg_diff_k(data, 3)
+    return r1, r2
 
 
 def day2(input_file_path):
-    """DAY 2 - 7 LINES - https://adventofcode.com/2021/day/2
+    """DAY 2 - 4 LINES - https://adventofcode.com/2021/day/2
 
     In the second problem, it only changes the meaning of depth. In the first problem depth is really aim.
     """
-    data = tuple(map(lambda l: l.strip().split(" "), open(input_file_path, 'r')))
-    totals = defaultdict(int)
-    effect = {'down': [1, 0, 0], 'up': [-1, 0, 0], 'forward': [0, 1, 1]}
+    totals, effect, data = defaultdict(int), {'down': [1, 0, 0], 'up': [-1, 0, 0], 'forward': [0, 1, 1]}, tuple(map(lambda l: l.strip().split(" "), open(input_file_path, 'r')))
     for cmd, how_much in data:
-        for v, q in zip(['aim', 'forward', 'depth'], effect[cmd]):
-            totals[v] += q * int(how_much) * np.power(totals['aim'], int(v == 'depth'))
-    return totals['forward'] * totals['aim'], totals['forward'] * totals['depth']
+        for v, q in zip(['aim', 'forward', 'depth'], effect[cmd]): totals[v] += q * int(how_much) * np.power(totals['aim'], int(v == 'depth'))
+    r1, r2 = totals['forward'] * totals['aim'], totals['forward'] * totals['depth']
+    return r1, r2
 
 
 def day3(input_file_path):
@@ -50,11 +48,12 @@ def day3(input_file_path):
     def gamma(subset, q): return np.array((np.sum(subset, axis=0) >= (subset.shape[0] / 2.)) == q, dtype=np.int)
     def bin2dec(n): return int("".join(map(str, n)), 2)
     def f2(subset, p, q): return bin2dec(subset[0]) if subset.shape[0] == 1 else f2(subset[subset[:, p] == gamma(subset, q)[p], :], p+1, q)
-    return bin2dec(gamma(data, 1)) * bin2dec(gamma(data, 0)), f2(data, 0, 1) * f2(data, 0, 0)
+    r1, r2 = bin2dec(gamma(data, 1)) * bin2dec(gamma(data, 0)), f2(data, 0, 1) * f2(data, 0, 0)
+    return r1, r2
 
 
 def day4(input_file_path):
-    """DAY 4 - ? LINES - https://adventofcode.com/2021/day/4
+    """DAY 4 - 10 LINES - https://adventofcode.com/2021/day/4
     """
     with open(input_file_path, 'r') as f:
         drawn = f.readline().strip().split(",")
@@ -77,17 +76,18 @@ def day5(input_file_path):
     for line in data:
         diff_x, diff_y, steps = line[2] - line[0], line[3] - line[1], max(abs(line[2] - line[0]), abs(line[3] - line[1]))
         for k in range(steps+1): F[int(line[1] + k * diff_y / steps), int(line[0] + k * diff_x / steps), int(diff_x == 0 or diff_y == 0)] += 1
-    return np.sum(F[:, :, 1] > 1), np.sum(np.sum(F, axis=-1) > 1)
+    r1, r2 = np.sum(F[:, :, 1] > 1), np.sum(np.sum(F, axis=-1) > 1)
+    return r1, r2
 
 
 def day6(input_file_path):
     """DAY 6 - 5 LINES - https://adventofcode.com/2021/day/6
     """
-    r1, data = 0, [np.sum(np.array(list(map(int, open(input_file_path, 'r').read().split(",")))) == i).astype('int64') for i in range(9)]
+    r1, r2, data = 0, 0, [np.sum(np.array(list(map(int, open(input_file_path, 'r').read().split(",")))) == i).astype('int64') for i in range(9)]
     for day in range(256):
         data = data[1:7] + [data[7] + data[0]] + [data[8]] + [data[0]]
-        if day < 80: r1 = sum(data)
-    return r1, sum(data)
+        r1, r2 = sum(data) if day < 80 else r1, sum(data)
+    return r1, r2
 
 
 def day7(input_file_path):
@@ -95,14 +95,15 @@ def day7(input_file_path):
     """
     data = np.array(list(map(int, open(input_file_path, 'r').read().split(","))))
     def res(input, fun): return min([fun(np.abs(input - i)).sum() for i in range(input.min(), input.max())])
-    return res(data, fun=lambda x: x), res(data, fun=lambda x: x * (x + 1) // 2)
+    r1, r2 = res(data, fun=lambda x: x), res(data, fun=lambda x: x * (x + 1) // 2)
+    return r1, r2
 
 
 if __name__ == "__main__":
     # print('DAY 1: ' + str(day1('input_1.txt')))
     # print('DAY 2: ' + str(day2('input_2.txt')))
-    print('DAY 3: ' + str(day3('input_3.txt')))
+    # print('DAY 3: ' + str(day3('input_3.txt')))
     # print('DAY 4: ' + str(day4('input_4.txt')))
     # print('DAY 5: ' + str(day5('input_5.txt')))
     # print('DAY 6: ' + str(day6('input_6.txt')))
-    # print('DAY 7: ' + str(day7('input_7.txt')))
+    print('DAY 7: ' + str(day7('input_7.txt')))
